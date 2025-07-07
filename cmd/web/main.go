6f9c4b48556ccc884b84bf5de9af8e3d38dd565a
Земсков Матвей <zemskov@citgk.ru>
2025-07-07 16:15:46 +0300

    Creating a database connection and test it

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type application struct {
@@ -15,6 +18,7 @@ type application struct {
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 func main() {
@@ -23,12 +27,23 @@ func main() {
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to static assets")
+	// user:pass@server/db_name
+	flag.StringVar(&cfg.dsn, "dsn", "mattweb_snipbox:c5T9iBFD@tcp(5.23.51.100:3306)/mattweb_snipbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level:     slog.LevelDebug,
 		AddSource: true,
 	}))
+
+	db, err := openDB(cfg.dsn)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		Logger: logger,
@@ -37,7 +52,24 @@ func main() {
 
 	logger.Info("starting server", "addr", cfg.addr)
 
-	err := http.ListenAndServe(cfg.addr, app.routes())
+	err = http.ListenAndServe(cfg.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
